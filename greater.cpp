2a8767f7f5241ad751f27d2c943be0235624877e
ODHRUBO <omar.faruk.csecu@gmail.com>
2024-05-28 17:16:11 +0600

    one array program is added

---
+++ greater.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cin>>a >>b;
+  if(a>b){
+    cout<<a <<" is greate then "<<b<<endl;
+  }
+  else if(a<b){
+    cout<<a<<" is less then "<<b<<endl;
+  }else {
+    {
+      cout<<a<<" is equals to "<<b<<endl;
+    }
+  }
+}
